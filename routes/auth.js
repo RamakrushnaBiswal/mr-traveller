03bd6961b2794a7f5a478277d5ffd5bfb1787280
RamakrushnaBiswal <ramakrushnabunty@gmail.com>
2024-04-14 10:11:46 +0530

    added

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,8 @@ const FederatedCredentials  = require('../models/googleuser');
 passport.use(new GoogleStrategy({
   clientID: process.env['GOOGLE_CLIENT_ID'],
   clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-  callbackURL: 'http://localhost:3000/oauth2/redirect/google',
+  // callbackURL: 'http://localhost:3000/oauth2/redirect/google', //for devlopment
+  callbackURL:'https://mr-traveller.onrender.com/oauth2/redirect/google',
   scope: ['profile']
 }, function verify(issuer, profile, cb) {
   // Check if the user already exists
